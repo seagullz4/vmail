290f69ba363efb761c2a8661855e5ef1df2828c9
seagullz4 <seagullz4@users.noreply.github.com>
2026-06-04 17:06:59 +0800

    UI: change brand logo to baibai and remove footer copyright text

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -7,19 +7,8 @@ export function Footer() {
   return (
     <div className="text-white w-full mt-auto flex flex-col items-center justify-between px-5 pt-16 mb-10 md:px-10 mx-auto sm:flex-row">
       <Link to="/" className="text-xl font-black leading-none select-none logo">
-        VMAIL.DEV
-      </Link>{" "}
-      <p className="mt-4 text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">
-        © 2024-2026 Products of{" "}
-        <a
-          className="font-semibold underline hover:text-gray-600"
-          href="https://www.oiov.dev"
-          target="_blank"
-          rel="noopener noreferrer">
-          oiov
-        </a>
-        .
-      </p>
+        baibai
+      </Link>
     </div>
   );
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -72,7 +72,7 @@ export function Header() {
           className="font-bold flex items-center justify-center gap-3">
           <VmailLogo />
           <button className="cool-btn">
-            <span>VMAIL.DEV</span>
+            <span>baibai</span>
           </button>
         </Link>
         <nav className="flex items-center">
